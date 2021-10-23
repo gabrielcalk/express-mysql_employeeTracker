22cd6878639101562cd59696c416bd53764d1480
unknown <gabrielcalk@outlook.com>
2021-10-23 00:05:40 -0400

    write all the codes to create and insert data to the ddatabase

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,31 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE departments_db
+CREATE TABLE departments_db(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
 
-CREATE TABLE 
+CREATE TABLE roles_db(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments_db(id)
+);
+
+DROP TABLE employees_db
+CREATE TABLE employees_db(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles_db(id),
+    primary key (id, manager_id)
+    -- composite primary key
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO departments_db
+VALUES  ('Marketing'),
+        ('Finance'),
+        ('Product');
+
+INSERT INTO roles_db (title, salary, department_id)
+VALUES  ('Head of Marketing', 120000, 1),,
+        ('Designer', 80000, 1),
+        ('Traffic Manager', 110000, 1),
+        ('Head of Accounting', 90000, 2),
+        ('Assistant of Accounting', 50000, 2),
+        ('Senior Front End Engineer', 190000, 3),
+        ('Senior Full Stack Engineer' 220000, 3),
+        ('Software Architect', 140000, 3);
+
+INSERT INTO employees_db (first_name, last_name, role_id, manager_id)
+VALUES  ('Ana', 'Orth', 1, 1),
+        ('Cristiane', 'Souza', 4, 1),
+        ('Cesar', 'Orth', 2, 0),
+        ('Pedro', 'Orth', 5, 0);
